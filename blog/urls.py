d56d2bf585fb6c312f456e2dcdac2cec66926db8
Murasakibara990 <xurshidst2006@gmail.com>
2024-02-16 19:19:46 +0500

    'Full'

--- blog/urls.py
+++ blog/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('categories/', Category_API_view.as_view(), name='categories'),
     path('tags/', Tag_API_view.as_view(), name='tags'),
     path('authors/', Author_API_view.as_view(), name='authors'),
-    path('happy-clients/', Subscribe_serializer.as_view(), name='clients'),
+    path('subscribe/', Subscribe_serializer.as_view(), name='subscribes'),
     path('comments/', Comment_APIV_view.as_view(), name='comments'),
     path('recent/', Recent_blogs_API_view.as_view(), name='recent-posts'),
 ]
