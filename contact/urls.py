6888b83a53abf9a88395803ee3b7f57690e4cadb
asadbek <asadashirkulov555@gmail.com>
2024-02-19 20:42:06 +0500

    first commit

--- contact/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import ContactAPIView, ContactInfoAPIView
-urlpatterns = [
-    path('', ContactAPIView.as_view(), name='contact'),
-    path('info/', ContactInfoAPIView.as_view(), name='info')
-
-]
